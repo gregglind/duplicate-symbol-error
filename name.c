bd983c35552a0deed46ad961585288c901446dae
Gregg Lind <gregg.lind@gmail.com>
2014-08-07 09:54:44 -0500

    Resolved by moving definition from .h -> .c

--- name.c
+++ name.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "name.h"
 
+char* name = "Heisenberg";
+
 void say_name() {
     printf("%s\n", name);
 }
--- name.h
+++ name.h
@@ -1,7 +1,6 @@
 #ifndef NAME_H
 #define NAME_H
 
-char* name = "Heisenberg";
 void say_name();
 
 #endif
